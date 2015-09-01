209a76df3155825af5c47f7047d145d76da5568f
S. M. Abrar Jahin <abrarjahin@live.com>
2015-09-01 09:11:56 +0600

    Done for Option and popup Menu

--- app/src/main/java/com/appnucleus/custommenu/ActivityMain.java
+++ app/src/main/java/com/appnucleus/custommenu/ActivityMain.java
@@ -4,22 +4,59 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 import android.widget.Toast;
 
-public class ActivityMain extends AppCompatActivity
+public class ActivityMain extends AppCompatActivity implements OnMenuItemClickListener
 {
+    ////////////////////////////////////////////////////////////////////////////Show Popup Menu
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_click).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                PopupMenu popupMenu = new PopupMenu(ActivityMain.this, view);
+                popupMenu.setOnMenuItemClickListener(ActivityMain.this);
+                popupMenu.inflate(R.menu.menu_popup);
+                popupMenu.show();
+            }
+        });
     }
 
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.item_comedy:
+                Toast.makeText(this, "Comedy Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_movies:
+                Toast.makeText(this, "Movies Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item_music:
+                Toast.makeText(this, "Music Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
+    }
+    ////////////////////////////////////////////////////////////////////////////Popup Menu End
+
+
+    //////////////////////////////////////////////////////////////////////////////For Option Menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        getMenuInflater().inflate(R.menu.menu_option, menu);
         return true;
     }
 
@@ -57,4 +94,5 @@ public class ActivityMain extends AppCompatActivity
                 return super.onOptionsItemSelected(item);
         }
     }
+    ///////////////////////////////////////////////////////////////////////////////Option Menu End
 }
